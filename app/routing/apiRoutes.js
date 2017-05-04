3b6ef6362f7bf3c6732ae049ad4b9e82efcc447c
Stacy Ann Marshall <stacy.marshall@northwestern.edu>
2017-05-04 15:08:06 -0500

    working on array comparisons

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,11 +1,36 @@
-module.exports = function(app) {
+module.exports = function(app, friends) {
     app.get("/api/friends/", function(req, res) {
         return res.json(friends);
     });
 
     app.post("/api/new", function(req, res) {
+
         var newFriend = req.body;
         friends.push(newFriend);
         res.json(newFriend);
+        var newFriendScores = newFriend.scores;
+        var arrayLocation = (friends.length - 1);
+
+
+        if (friends.length === 1) {
+            console.log('You are the first applicant!');
+        } else {
+            for (var i = 0; i < friends.length; i++) {
+                if (i !== arrayLocation) {
+                    var oldFriendScores = friends[i].scores;
+                    var differenceArray = newFriendScores.map(function(num, i) {
+                        return Math.abs(num - oldFriendScores[i]);
+                    })
+
+                    console.log('Sum: ' + newFriend.name + ' & ' + friends[i].name + ' array ' + differenceArray);
+
+                    var sum = differenceArray.reduce(function(a, b) {
+                        return a + b;
+                    }, 0);
+                    console.log('Sum: ' + sum);
+                }
+            }
+        }
+
     });
 }
--- server.js
+++ server.js
@@ -13,10 +13,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(express.static(__dirname + "/public"));
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
 });
 
 htmlRoutes(app);
-apiRoutes(app);
+apiRoutes(app, friends);
