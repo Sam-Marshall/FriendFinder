da74f46eea6ea1a6791e111a2a43866543d1a9e8
Stacy Ann Marshall <stacy.marshall@northwestern.edu>
2017-05-04 18:25:25 -0500

    modal is functional. Added home button and api links to survey page

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,30 +7,51 @@ module.exports = function(app, friends) {
 
         var newFriend = req.body;
         friends.push(newFriend);
-        res.json(newFriend);
         var newFriendScores = newFriend.scores;
         var arrayLocation = (friends.length - 1);
-
+        var arrayOfSums = [];
+        var message = '';
 
         if (friends.length === 1) {
-            console.log('You are the first applicant!');
+            message = 'You are the first applicant!';
         } else {
             for (var i = 0; i < friends.length; i++) {
+
                 if (i !== arrayLocation) {
                     var oldFriendScores = friends[i].scores;
                     var differenceArray = newFriendScores.map(function(num, i) {
                         return Math.abs(num - oldFriendScores[i]);
                     })
 
-                    console.log('Sum: ' + newFriend.name + ' & ' + friends[i].name + ' array ' + differenceArray);
+                    console.log(newFriend.name + ' & ' + friends[i].name + ' array ' + differenceArray);
 
                     var sum = differenceArray.reduce(function(a, b) {
                         return a + b;
                     }, 0);
-                    console.log('Sum: ' + sum);
-                }
-            }
-        }
 
+                    arrayOfSums.push(sum);
+
+                    console.log('Sum: ' + sum);
+
+                } else {
+                    arrayOfSums.push(1000);
+                }
+
+            }
+            console.log(arrayOfSums);
+
+            var indexOfSmallest = (arrayOfSums.length) - 1;
+            console.log(indexOfSmallest);
+
+            for (var i = 0; i < arrayOfSums.length; i++) {
+
+                if (arrayOfSums[i] < arrayOfSums[indexOfSmallest]) {
+                    indexOfSmallest = i;
+                }
+
+            }
+            message = friends[indexOfSmallest].name + ' is your new best friend';
+        }
+        res.json(friends[indexOfSmallest]);
     });
 }
