31de843815ccb46a18459788584c27c300f34b16
Stacy Ann Marshall <stacy.marshall@northwestern.edu>
2017-05-06 09:45:25 -0500

    working on possible port/heroku problem

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ var apiRoutes = require('./app/routing/apiRoutes');
 var friends = [];
 
 var app = express();
-var PORT = 3000;
+app.set('port', (process.env.PORT || 5000));
+// var PORT = 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -16,8 +17,8 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 app.use(express.static(__dirname + "/app/public"));
 
-app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+app.listen(app.get('port'), function() {
+    console.log("App listening on PORT " + app.get('port'));
 });
 
 htmlRoutes(app);
