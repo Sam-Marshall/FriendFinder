3311bc689e3f803edc76f45b74e8cc8aca628124
Stacy Ann Marshall <stacy.marshall@northwestern.edu>
2017-05-12 09:58:38 -0500

    removed composer.json and index.php, trying to get heroku error to resolve

--- server.js
+++ server.js
@@ -2,13 +2,13 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
-var htmlRoutes = require('./app/routing/htmlRoutes');
-var apiRoutes = require('./app/routing/apiRoutes');
+
 var friends = [];
 
 var app = express();
+
 app.set('port', (process.env.PORT || 5000));
-// var PORT = 3000;
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -21,5 +21,5 @@ app.listen(app.get('port'), function() {
     console.log("App listening on PORT " + app.get('port'));
 });
 
-htmlRoutes(app);
-apiRoutes(app, friends);
+require('./app/routing/htmlRoutes')(app);
+require('./app/routing/apiRoutes')(app, friends);
